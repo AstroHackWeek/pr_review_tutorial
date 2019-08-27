c19adcccc59255e67cfdbbb0db8dddc3c9add4f2
Oliver Hall <ojh251@student.bham.ac.uk>
2019-08-27 09:38:40 +0100

    Added a docstring, input hint and input test to simple_functions.factorial()

--- Hall/template/simple_functions.py
+++ Hall/template/simple_functions.py
@@ -5,7 +5,28 @@ def fibonacci(max):
     return values
 
 
-def factorial(value):
+def factorial(value: int):
+    """ A function that returns the factorial of the input value. The factorial
+    is calculated by recusrively multiplying with each integer below the input
+    value.
+
+    Input
+    ------
+    value : int
+        The value for which the factorial is calculated, eg: 4 would return 4!
+        Must be an positive integer.
+
+    Returns
+    ------
+    factorial : int
+        The factorial of the input value.
+    """
+    if type(value) is not int:
+        raise ValueError('Value must be an integer.')
+
+    if value < 0:
+        raise ValueError('Value must be a positive integer.')
+
     if value == 0:
         return 1
     else:
