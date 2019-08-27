c68c3efa31f19b418fad93ca6cd9dbc65de7592f
Luis Peralta <lp511@cam.ac.uk>
2019-08-27 11:57:57 +0100

    is_prime function improved to consider numbers lower than 1 and its efficency

--- Peralta/template/simple_functions.py
+++ Peralta/template/simple_functions.py
@@ -13,16 +13,22 @@ def factorial(value):
 
 def is_prime(value):
     assert type(value) is int
-    
-    # Initialise the result as True
-    result = True
 
-    # Try to find a integer which divides the input value:
-    # If we find one, the result will be changed to False
-    for i in range(2, value):
-        if value % i == 0:
-            result = False
-            break
+    if value > 1:
+        # Initialise the result as True
+        result = True
+
+        # Try to find a integer which divides the input value:
+        # If we find one, the result will be changed to False
+        
+        max_test_int = math.floor(math.sqrt(value))
+
+        for i in range(2, max_test_int + 1):
+            if value % i == 0:
+                result = False
+                break
+    else:
+        result = False
 
     return result
 
