925295c986825955050dadf14ef2f8befa3e9679
M Atakan Gürkan <ato.gurkan@gmail.com>
2019-08-27 11:00:40 +0100

    fixed docstring for is_prime()

--- Gurkan/template/simple_functions.py
+++ Gurkan/template/simple_functions.py
@@ -17,9 +17,22 @@ def factorial(value):
 
 
 def is_prime(n):
-    ''' This function checks whether the arguments is a prime number.
+    '''
+    A function to check whether the argument is a prime number.
+
+    This function uses the sieve of Eratoshenes to check whether its
+    input is a prime number.
     It expects an integer, but it can be represented as a float, e.g.,
     7.0 is OK, but 7.1 is not.
+
+    Parameters
+    ----------
+    n : integer
+        The input.
+    Returns
+    -------
+    Boolean
+        True if the input is a prime, False otherwise.
     '''
     if floor(n) != n:
         raise OSError('input to is_prime() should be an integer!')
