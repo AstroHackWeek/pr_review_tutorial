51186f85441190e1d3d7a47a3696ae7b6b7fed59
jan <jcjvanroestel@gmail.com>
2019-08-27 10:00:04 +0100

    added comments to isprime code

--- Roestel/template/simple_functions.py
+++ Roestel/template/simple_functions.py
@@ -13,10 +13,24 @@ def factorial(value):
 
 
 def isprime(n):
+    """Test if an a number is a prime. Method from 
+    https://en.wikipedia.org/wiki/Prime_number
+
+    input:
+        int n : input integer to test
+    output:
+        bool : True/False for n is prime
+    """
+    
+    # check small numbers first
     if n < 4:
         return True
+
+    # check if it can be divided by 2 or 3
     if n%2==0 or n%3==0:
         return False
+
+    # check rest
     i = 5
     while i**2<=n:
         if n%i==0 or n%(i+2)==0:
