2b10ff14836a7c1c29b6350be6474bc35854b13a
carol <florpisoteadadejardin@gmail.com>
2019-08-27 09:30:02 +0100

    Added is prime

--- Cuesta/template/simple_functions.py
+++ Cuesta/template/simple_functions.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def fibonacci(max):
     values = [0, 1]
     while values[-2] + values[-1] < max:
@@ -10,3 +12,28 @@ def factorial(value):
         return 1
     else:
         return value * factorial(value - 1)
+
+def test_isPrime():
+
+    assert isPrime(3) == True
+    assert isPrime(5) == True
+    assert isPrime(12) == False
+
+def isPrime(value):
+
+
+    for i in range(2, int(np.sqrt(value))+ 1):
+
+        if (value % i) == 0:
+            return False 
+        else:
+            return True 
+
+
+    return True
+    
+
+
+if __name__ == "__main__":
+
+    print(isPrime(5))
