a9dfb94c5fa078e0aeaace13e40640d63c3f8733
jan <jcjvanroestel@gmail.com>
2019-08-27 09:41:53 +0100

    Added isprime function to simple_functions

--- Roestel/template/simple_functions.py
+++ Roestel/template/simple_functions.py
@@ -10,3 +10,16 @@ def factorial(value):
         return 1
     else:
         return value * factorial(value - 1)
+
+
+def isprime(n):
+    if n < 4:
+        return True
+    if n%2==0 or n%3==0:
+        return False
+    i = 5
+    while i**2<=n:
+        if n%i==0 or n%(i+2)==0:
+            return False
+        i+=6
+    return True 
